c2746367065a563da6eb157db03ab3e55d601846
Smiley Mak <smileymak2@gmail.com>
2021-06-13 17:39:03 +0800

    reviews_analytics2

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -12,3 +12,13 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('留言的平均長度為', sum_len / len(data), '筆')
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print('共有', len(new[0]), '字母')
+print(new[5])
+print('共有', len(new[5]), '字母')
